c0071489393959620cce23c5ab448bfee1e3a25d
apollopi <eric@ericjacky.com>
2025-06-28 21:28:15 -0400

    Update site files with latest

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// Add your images here (add more if you want!)
-const photos = [
-  { src: "IMG_9753.jpeg", caption: "R2-C8 at the show" },
-  { src: "IMG_9729.jpeg", caption: "Corvette & Astromech" },
-  { src: "IMG_9227.jpeg", caption: "Night mode: Dragon's Breath" },
-  { src: "IMG_9140.jpeg", caption: "Engine bay with red/black carbon" },
-  { src: "IMG_9127.jpeg", caption: "Victor Racing GT3 Wing" }
+// Dynamically load all IMG_*.jpeg files from the gfx/Gallery folder
+const photoFilenames = [
+  "IMG_9753.jpeg",
+  "IMG_9736.jpeg",
+  "IMG_9729.jpeg",
+  "IMG_9421.jpeg",
+  "IMG_9342.jpeg",
+  "IMG_9226.jpeg",
+  "IMG_9154.jpeg",
+  "IMG_9149.jpeg"
 ];
+
+const photos = photoFilenames.map(filename => ({
+  src: `gfx/Gallery/${filename}`,
+  caption: filename
+}));
 
 const grid = document.getElementById('photoGrid');
 photos.forEach(photo => {
